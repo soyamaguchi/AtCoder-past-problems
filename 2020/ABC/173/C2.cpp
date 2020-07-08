d7f483f4f83f47229c8972998f60d8e3aa2eb187
Soya Yamaguchi <f.soya.maguchi@gmail.com>
2020-07-08 23:18:34 +0900

    Add ABC173(A,B,C,D)

---
+++ 2020/ABC/173/C2.cpp
@@ -0,0 +1,101 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <bitset>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <complex>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <cassert>
+#include <fstream>
+#include <utility>
+#include <functional>
+#include <time.h>
+#include <stack>
+#include <array>
+#include <list>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector< vector<int> >
+#define vll vector< vector<ll> >
+#define vs vector<string>
+#define pii pair<int,int>
+#define pis pair<int,string>
+#define psi pair<string,int>
+#define pll pair<ll,ll>
+#define mii map<int,int>
+#define mis map<int,string>
+#define msi map<string,int>
+#define mll map<ll,ll>
+#define rep(i,n) for(int i=0; i<(int)(n); i++)
+#define rep1(i,n) for(int i=1; i<=(int)(n); i++)
+#define all(c) c.begin(),c.end()
+#define srta(a) sort(all(a))
+#define srtd(a) sort(all(a),greater<型>())
+#define rvs(s) reverse(all(s))
+#define psb push_back
+#define ppb pop_back
+#define isu isupper
+#define isl islower
+#define lb(a,k) lower_bound(all(a),k)
+#define ub(a,k) upper_bound(all(a),k)
+#define bs(a,k) binary_search(all(a),k)
+
+const int I_INF = 1001001001;
+const ll LL_INF = 1001001001001001001;
+const ld EPS = 0.001;
+
+
+
+int main() {
+    int H, W, K;
+    cin >> H >> W >> K;
+
+    vii c(H, vi(W));
+    rep (i, H) {
+        rep (j, W) {
+            char s;
+            cin >> s;
+            c[i][j] = s == '.' ? 0 : 1;
+        }
+    }
+
+    int cnt = 0;
+    rep (i, 1 << (H+W)) {
+        vii c2 = c;
+        rep (j, H) {
+            if ((i >> j) & 1) {
+                rep (k, W) c2[j][k] = 2;
+            }
+        }
+        rep (j, W) {
+            if ((i >> (j+H)) & 1) {
+                rep (k, H) c2[k][j] = 2;
+            }
+        }
+        int tmp_cnt = 0;
+        rep (j, H) {
+            rep (k, W) {
+                if (c2[j][k] == 1) tmp_cnt++;
+            }
+        }
+        if (tmp_cnt == K) cnt++;
+    }
+
+    cout << cnt << endl;
+}
